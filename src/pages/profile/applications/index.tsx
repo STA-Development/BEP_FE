e7a78f5d23282a092eeaad05d8d67407a85b283a
Gor Khachatryan <gorkhachatryan@MacBook-Pro.local>
2023-03-29 02:59:40 +0400

    Resolve Arthur's comments

--- src/pages/profile/applications/index.tsx
+++ src/pages/profile/applications/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { ModalName } from '@allTypes/modals'
 import { Button } from '@components/Button'
 import { AddIcon, DeleteIcon } from '@components/Icons'
@@ -42,71 +42,78 @@ const applications = [
   },
 ]
 
-export const Applications = () => (
-  <>
-    {applications.map((item) => (
-      <div className="mb-10 rounded border border-gray-light p-10">
-        <h2 className="mb-5 text-lg">{item.label}:</h2>
-        <div className="mb-10 flex">
-          {item.statuses.map((status) => (
-            <p
-              key={status.label}
-              className="mr-10 text-base text-black-light"
-            >
-              {status.label}:<span className="ml-5 font-medium text-primary">{status.status}</span>
-            </p>
-          ))}
-        </div>
-        <div className="flex justify-between">
-          <div className="flex gap-10">
+export const Applications = () => {
+  const onOpenAddApplicationModal = useCallback(() => {
+    dispatch(
+      viewsMiddleware.openModal({
+        name: ModalName.AddApplicationModal,
+        props: {},
+      })
+    )
+  }, [])
+
+  const onDeleteAddApplicationModal = useCallback(() => {
+    dispatch(
+      viewsMiddleware.openModal({
+        name: ModalName.DeleteApplicationModal,
+        props: {},
+      })
+    )
+  }, [])
+
+  return (
+    <>
+      {applications.map((item) => (
+        <div className="mb-10 rounded border border-gray-light p-10">
+          <h2 className="mb-5 text-lg">{item.label}:</h2>
+          <div className="mb-10 flex">
+            {item.statuses.map((status) => (
+              <p
+                key={status.label}
+                className="mr-10 text-base text-black-light"
+              >
+                {status.label}:
+                <span className="ml-5 font-medium text-primary">{status.status}</span>
+              </p>
+            ))}
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-10">
+              <Button
+                size="bs"
+                variant="outlined"
+              >
+                Deactivate Application
+              </Button>
+              <Button
+                size="bs"
+                variant="contained"
+              >
+                Edit Form
+              </Button>
+            </div>
             <Button
-              size="bs"
-              variant="outlined"
+              variant="text"
+              leftIcon={<DeleteIcon />}
+              onClick={onDeleteAddApplicationModal}
             >
-              Deactivate Application
-            </Button>
-            <Button
-              size="bs"
-              variant="contained"
-            >
-              Edit Form
+              Delete Application
             </Button>
           </div>
-          <Button
-            variant="text"
-            leftIcon={<DeleteIcon />}
-            onClick={() =>
-              dispatch(
-                viewsMiddleware.openModal({
-                  name: ModalName.DeleteApplicationModal,
-                  props: {},
-                })
-              )
-            }
-          >
-            Delete Application
-          </Button>
         </div>
-      </div>
-    ))}
-    <Button
-      variant="contained"
-      color="gray"
-      size="hg"
-      leftIcon={<AddIcon />}
-      onClick={() =>
-        dispatch(
-          viewsMiddleware.openModal({
-            name: ModalName.AddApplicationModal,
-            props: {},
-          })
-        )
-      }
-    >
-      Add Application
-    </Button>
-  </>
-)
+      ))}
+      <Button
+        variant="contained"
+        color="gray"
+        size="hg"
+        leftIcon={<AddIcon />}
+        onClick={onOpenAddApplicationModal}
+      >
+        Add Application
+      </Button>
+    </>
+  )
+}
 
 export default Applications
 Applications.Layout = 'Profile'
