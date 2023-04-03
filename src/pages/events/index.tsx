0db9816efc5d58cb03e01ab507064b1a8d028140
alexkocharyan01 <alexkocharyan97@gmail.com>
2023-04-03 19:15:01 +0400

    remuve constant show more and function showmoreinfo and change style

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Button } from '@components/Button'
 import { Container } from '@components/Container'
 import { RightIcon } from '@components/Icons'
+import { PageHeader } from '@components/PageHeader'
 import Image from 'next/image'
 
 const data = [
@@ -32,20 +33,10 @@ const data = [
 ]
 
 const News = () => {
-  const [showMore, setShowMore] = useState<boolean>(false)
   const [showModeId, setShowModeId] = useState<number>()
 
   const handleShowMore = (id: number) => {
     setShowModeId(id)
-    setShowMore(true)
-  }
-
-  const showMoreInfo = (id: number) => {
-    if (showMore && id === showModeId) {
-      return true
-    }
-
-    return false
   }
 
   return (
@@ -53,7 +44,7 @@ const News = () => {
       <div className="border-b border-gray-thin">
         <div className="mt-5 flex w-full justify-between border-b border-gray-thin pb-5 xl:pb-10">
           <div>
-            <h1 className="text-xl text-black ">Events</h1>
+            <PageHeader title="Events" />
             <p className="mt-2.5 text-base text-black-light xl:mt-5 xl:text-lg">
               Keep up with the following events
             </p>
@@ -67,7 +58,7 @@ const News = () => {
             key={item.id}
             className="hover:rounded-2.5 mt-5 flex flex-col rounded border border-gray-thin p-10 hover:border-primary xl:mt-10 xl:flex-row xl:items-start xl:justify-between"
           >
-            <div className="xl:w-[35%]">
+            <div className="xl:w-[500px]">
               <Image
                 src={item.image}
                 width={500}
@@ -76,7 +67,7 @@ const News = () => {
                 className="h-full w-full"
               />
               <div className="hidden xl:flex">
-                {item.secondInfo && showMoreInfo(item.id) ? (
+                {item.secondInfo && item.id === showModeId ? (
                   <p className="mt-5 text-base text-black-light">{item.secondInfo}</p>
                 ) : null}
               </div>
@@ -84,17 +75,17 @@ const News = () => {
             <div className="xl:w-[60%]">
               <h2 className="mt-5 text-xl text-black xl:mt-0 xl:text-2xl">{item.time}</h2>
               <p className="mt-2.5 text-lg text-black">{item.title}</p>
-              {item.secondInfo && showMoreInfo(item.id) ? (
+              {item.secondInfo && item.id === showModeId ? (
                 <p className="text-base text-black-light">{item.secondInfo}</p>
               ) : null}
               <p className="text-base text-black-light xl:mt-2.5">
-                {showMoreInfo(item.id) ? item.info : `${item.info.substring(0, 130)}`}
+                {item.id === showModeId ? item.info : `${item.info.substring(0, 130)}`}
               </p>
               <p className="mt-5 text-sm text-black-light xl:hidden">{item.time}</p>
               <div className="mb-5 mt-5 xl:hidden">
-                {showMoreInfo(item.id) ? (
+                {item.id === showModeId ? (
                   <Button
-                    onClick={() => setShowMore(false)}
+                    onClick={() => setShowModeId(0)}
                     variant="outlined"
                     className="w-full xl:hidden xl:pl-0"
                   >
@@ -111,9 +102,9 @@ const News = () => {
               </div>
               {!item.isButtonVisible ? (
                 <div>
-                  {showMoreInfo(item.id) ? (
+                  {item.id === showModeId ? (
                     <Button
-                      onClick={() => setShowMore(false)}
+                      onClick={() => setShowModeId(0)}
                       variant="outlined"
                       className="hidden w-full pl-0 xl:mt-[30px] xl:flex"
                     >
