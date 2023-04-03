7afbf3339102e4e8e202b581aad53c4806665556
alexkocharyan01 <alexkocharyan97@gmail.com>
2023-04-03 18:59:10 +0400

    A few changes change button structure

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -11,7 +11,7 @@ const data = [
     info: 'Lorem ipsum dolor sit amet consectetur. Tincidunt nisi pretium congue tristique egestas tincidunt. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse.',
     time: '31.01.2022',
     image: '/image1.jpg',
-    isButtonVisuable: true,
+    isButtonVisible: true,
   },
   {
     id: 2,
@@ -26,8 +26,6 @@ const data = [
     id: 3,
     title: 'Visit Of The Training Center Of The Chamber',
     info: 'Lorem ipsum dolor sit amet consectetur. Tincidunt nisi pretium congue tristique egestas tincidunt. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse. Lorem ipsum dolor sit amet consectetur. Tincidunt nisi pretium congue tristique egestas tincidunt. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse. Lorem ipsum dolor sit amet consectetur. Tincidunt nisi pretium congue tristique egestas tincidunt. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse. Lorem ipsum dolor sit amet consectetur. Tincidunt nisi pretium congue tristique egestas tincidunt. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse.',
-    secondInfo:
-      'Montes sit mattis urna faucibus habitasse. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna.  turpis tempus eu. Montes sit mattis urna. Morbi lobortis purus risus turpis tempus eu. Montes sit mattis urna faucibus habitasse. Morbi lobortis purus risus turpis tempus eu.',
     time: '31.01.2022',
     image: '/image1.jpg',
   },
@@ -37,13 +35,9 @@ const News = () => {
   const [showMore, setShowMore] = useState<boolean>(false)
   const [showModeId, setShowModeId] = useState<number>()
 
-  const handleShowMore = (id: number, variant?: string) => {
-    if (variant === 'Open') {
-      setShowModeId(id)
-      setShowMore(true)
-    } else if (variant === 'Close') {
-      setShowMore(false)
-    }
+  const handleShowMore = (id: number) => {
+    setShowModeId(id)
+    setShowMore(true)
   }
 
   const showMoreInfo = (id: number) => {
@@ -55,7 +49,7 @@ const News = () => {
   }
 
   return (
-    <Container className="xl:pb=[182px] pb-[152px]">
+    <Container className="xl:pb=10 pb-10">
       <div className="border-b border-gray-thin">
         <div className="mt-5 flex w-full justify-between border-b border-gray-thin pb-5 xl:pb-10">
           <div>
@@ -71,7 +65,7 @@ const News = () => {
         {data.map((item) => (
           <div
             key={item.id}
-            className="hover:rounded-2.5 mt-5 flex flex-col rounded border border-gray-thin p-5 hover:border-primary xl:mt-[60px] xl:flex-row xl:items-start xl:justify-between"
+            className="hover:rounded-2.5 mt-5 flex flex-col rounded border border-gray-thin p-10 hover:border-primary xl:mt-10 xl:flex-row xl:items-start xl:justify-between"
           >
             <div className="xl:w-[35%]">
               <Image
@@ -81,45 +75,60 @@ const News = () => {
                 alt="picture"
                 className="h-full w-full"
               />
-              {showMore ? (
-                <div>
-                  {item.secondInfo && showModeId === item.id ? (
-                    <p className="mt-5 text-base text-black-light">{item.secondInfo}</p>
-                  ) : null}
-                </div>
-              ) : null}
+              <div className="hidden xl:flex">
+                {item.secondInfo && showMoreInfo(item.id) ? (
+                  <p className="mt-5 text-base text-black-light">{item.secondInfo}</p>
+                ) : null}
+              </div>
             </div>
             <div className="xl:w-[60%]">
               <h2 className="mt-5 text-xl text-black xl:mt-0 xl:text-2xl">{item.time}</h2>
               <p className="mt-2.5 text-lg text-black">{item.title}</p>
-
-              <p className="mt-2.5 text-base text-black-light">
+              {item.secondInfo && showMoreInfo(item.id) ? (
+                <p className="text-base text-black-light">{item.secondInfo}</p>
+              ) : null}
+              <p className="text-base text-black-light xl:mt-2.5">
                 {showMoreInfo(item.id) ? item.info : `${item.info.substring(0, 130)}`}
               </p>
               <p className="mt-5 text-sm text-black-light xl:hidden">{item.time}</p>
               <div className="mb-5 mt-5 xl:hidden">
-                <Button
-                  className="w-full xl:hidden xl:pl-0"
-                  onClick={() => handleShowMore(item.id, showMoreInfo(item.id) ? 'Close' : 'Open')}
-                  variant={showMoreInfo(item.id) ? 'outlined' : 'contained'}
-                >
-                  {showMoreInfo(item.id) ? 'Close' : 'Read All'}
-                </Button>
-              </div>
-              {!item.isButtonVisuable ? (
-                <div>
+                {showMoreInfo(item.id) ? (
                   <Button
-                    onClick={() =>
-                      handleShowMore(item.id, showMoreInfo(item.id) ? 'Close' : 'Open')
-                    }
-                    variant={showMoreInfo(item.id) ? 'outlined' : 'text'}
-                    rightIcon={showMoreInfo(item.id) ? null : <RightIcon fill="fill-primary" />}
-                    className={`hidden pl-0  xl:mt-[30px] xl:flex ${
-                      showMoreInfo(item.id) ? 'w-full' : ' '
-                    }`}
+                    onClick={() => setShowMore(false)}
+                    variant="outlined"
+                    className="w-full xl:hidden xl:pl-0"
                   >
-                    {showMoreInfo(item.id) ? 'Close' : 'Read more'}
+                    Close
                   </Button>
+                ) : (
+                  <Button
+                    className="w-full xl:hidden xl:pl-0"
+                    onClick={() => handleShowMore(item.id)}
+                  >
+                    Read All
+                  </Button>
+                )}
+              </div>
+              {!item.isButtonVisible ? (
+                <div>
+                  {showMoreInfo(item.id) ? (
+                    <Button
+                      onClick={() => setShowMore(false)}
+                      variant="outlined"
+                      className="hidden w-full pl-0 xl:mt-[30px] xl:flex"
+                    >
+                      Close
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={() => handleShowMore(item.id)}
+                      variant="text"
+                      rightIcon={<RightIcon fill="fill-primary" />}
+                      className="hidden pl-0 xl:mt-[30px] xl:flex"
+                    >
+                      Read more
+                    </Button>
+                  )}
                 </div>
               ) : null}
             </div>
