aa408cb87e9b215afff0801e619e778d60874430
Alex Kocharyan <49320774+alexkocharyan01@users.noreply.github.com>
2023-04-04 19:49:52 +0400

    FEAT: Events Page (#21)
    * Create Events page
    
    * A few changes in Events component
    
    * Fix git git conflict
    
    * A few changes change button structure
    
    * remuve constant show more and function showmoreinfo and change style
    
    * A few changes
    
    * A few changes
    
    * make changes in style
    
    * make changes in style
    
    * make changes in style
    
    * FIX: events page
    
    ---------
    
    Co-authored-by: Arsen Babajanyan <arsenbabajanyan95@gmail.com>

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
-export const Breadcrumbs = () => (
-  <ul className="flex text-base text-black-light ">
-    <li className="after:mx-2.5 after:content-['\/']">Home</li>
-    <li>Masters</li>
-  </ul>
-)
+export const Breadcrumbs = () => {
+  const location = useRouter()
+
+  const pageTitle = location.asPath.charAt(1).toUpperCase() + location.asPath.slice(2)
+
+  return (
+    <ul className="hidden text-base text-black-light xl:flex">
+      <li className="after:mx-2.5 after:content-['\/']">Home</li>
+      <li>{pageTitle}</li>
+    </ul>
+  )
+}
--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -3,13 +3,20 @@ import { Breadcrumbs } from '@components/Breadcrumbs'
 
 export const PageHeader = ({
   title,
+  description,
   breadcrumbs = false,
 }: {
   title: string
+  description?: string
   breadcrumbs?: boolean
 }) => (
-  <div className="mt-10 mb-7.5 flex items-center justify-between">
-    <h1 className="text-xl">{title}</h1>
+  <div className="my-5 flex w-full items-start justify-between xl:my-10">
+    <div>
+      <h1 className="text-xl font-medium xl:font-normal">{title}</h1>
+      {description ? (
+        <p className="mt-2.5 text-base text-black-light xl:mt-5 xl:text-lg">{description}</p>
+      ) : null}
+    </div>
     {breadcrumbs ? <Breadcrumbs /> : null}
   </div>
 )
