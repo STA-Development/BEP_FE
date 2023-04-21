bf71b44d78ae6ed49579f522904dfaac0d4d6e55
Arsen Babajanyan <arsenbabajanyan95@gmail.com>
2023-04-21 19:06:39 +0400

    [BF-67] FEAT: Update Header/Footer (#41)
    * FEAT: Update footer UI
    
    * FEAT: Update header navigation
    
    * FEAT: Update home Disclosure
    
    * FEAT: Update home UI and added wave to homepage
    
    * Fix: comments
    
    * FIX: mobile bars
    
    * FIX: language selector
    
    * FIX: language updates
    
    * FIX: remove unused dependency
    
    * FEAT: add wave

--- src/constants/translations.ts
+++ src/constants/translations.ts
@@ -1,6 +1,12 @@
 /* AUTO GENERATED FILE. DO NOT MODIFY !!! */
 
 export enum Translation {
+  FOOTER_COPYRIGHT = 'footer.copyright',
+  NAVBAR_ABOUT_US = 'navbar.about.us',
+  NAVBAR_CONTACT_US = 'navbar.contact.us',
+  NAVBAR_EDUCATIONAL_INSTITUTES = 'navbar.educational.institutes',
+  NAVBAR_HOME = 'navbar.home',
+  NAVBAR_LANGUAGE = 'navbar.language',
   HELP_BUTTON = 'help.button',
   HELP_HEADLINE = 'help.headline',
   HELP_HEADLINE_REQUIRED = 'help.headline.required',
@@ -8,8 +14,6 @@ export enum Translation {
   HELP_PROBLEM_REQUIRED = 'help.problem.required',
   HELP_TEL_PROBLEM = 'help.tel.problem',
   HELP_TEXT = 'help.text',
-  NAVBAR_ABOUT_US = 'navbar.about.us',
-  NAVBAR_HOME = 'navbar.home',
 }
 
 /* AUTO GENERATED FILE. DO NOT MODIFY !!! */
