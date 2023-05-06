0527cdf077c40f6df3b9ae7d101b27cd3a7f9bbe
Arthur02V <artur.voskanyan@stayopn.com>
2023-05-06 23:15:37 +0400

    FIX: build issue

--- src/pages/profile/monitoring-systems/index.tsx
+++ src/pages/profile/monitoring-systems/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { Translation } from '@constants/translations'
 import { Tab } from '@headlessui/react'
@@ -58,75 +59,81 @@ const years: Years[] = [
 const MonitoringSystems = () => {
   const [t] = useTranslation()
 
+  const methods = useForm()
+
   return (
-    <Tab.Group>
-      <Tab.List>
-        {tabs.map((tab) => (
-          <Tab
-            key={tab.label}
-            className="mr-10 text-base text-black focus:outline-none"
-          >
-            {({ selected }) => (
-              <>
-                <div className="p-2.5 pb-1.5">{tab.label}</div>
-                <div
-                  className={`h-1 w-full rounded ${selected ? 'bg-primary' : 'bg-transparent'}`}
-                />
-              </>
-            )}
-          </Tab>
-        ))}
-      </Tab.List>
-      <Tab.Panels>
-        <div className="grid grid-cols-1 divide-y divide-gray-thin">
-          <div className="my-10 inline-block xl:flex xl:w-full xl:flex-row">
-            <div className="flex flex-row gap-5">
-              <div className="w-[124px]">
-                <Autocomplete
-                  fieldName="years"
-                  items={years}
-                  placeholder={
-                    t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_YEARS) as string
-                  }
-                />
+    <FormProvider {...methods}>
+      <form>
+        <Tab.Group>
+          <Tab.List>
+            {tabs.map((tab) => (
+              <Tab
+                key={tab.label}
+                className="mr-10 text-base text-black focus:outline-none"
+              >
+                {({ selected }) => (
+                  <>
+                    <div className="p-2.5 pb-1.5">{tab.label}</div>
+                    <div
+                      className={`h-1 w-full rounded ${selected ? 'bg-primary' : 'bg-transparent'}`}
+                    />
+                  </>
+                )}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels>
+            <div className="grid grid-cols-1 divide-y divide-gray-thin">
+              <div className="my-10 inline-block xl:flex xl:w-full xl:flex-row">
+                <div className="flex flex-row gap-5">
+                  <div className="w-[124px]">
+                    <Autocomplete
+                      fieldName="years"
+                      items={years}
+                      placeholder={
+                        t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_YEARS) as string
+                      }
+                    />
+                  </div>
+                  <div className="w-[124px]">
+                    <Autocomplete
+                      fieldName="years"
+                      items={years}
+                      placeholder={
+                        t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_REGION) as string
+                      }
+                    />
+                  </div>
+                  <div className="w-[124px]">
+                    <Autocomplete
+                      fieldName="years"
+                      items={years}
+                      placeholder={
+                        t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_REGION) as string
+                      }
+                    />
+                  </div>
+                  <Button size="md">
+                    {t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_ACTIONS_APPLY_FILTERS)}
+                  </Button>
+                </div>
               </div>
-              <div className="w-[124px]">
-                <Autocomplete
-                  fieldName="years"
-                  items={years}
-                  placeholder={
-                    t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_REGION) as string
-                  }
-                />
-              </div>
-              <div className="w-[124px]">
-                <Autocomplete
-                  fieldName="years"
-                  items={years}
-                  placeholder={
-                    t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_FILTERS_REGION) as string
-                  }
-                />
-              </div>
-              <Button size="md">
-                {t(Translation.PAGE_PROFILE_MENU_MONITORING_SYSTEMS_ACTIONS_APPLY_FILTERS)}
-              </Button>
+              {tabs.map((tab) => (
+                <Tab.Panel
+                  key={tab.label}
+                  className="pt-10"
+                >
+                  <div className="mb-5 text-lg font-medium">{tab.title}</div>
+                  <div className="mb-5 font-normal">{tab.content}</div>
+                  <div className="mb-5 font-normal text-black-light">{tab.content2}</div>
+                  <div>{tab.content3}</div>
+                </Tab.Panel>
+              ))}
             </div>
-          </div>
-          {tabs.map((tab) => (
-            <Tab.Panel
-              key={tab.label}
-              className="pt-10"
-            >
-              <div className="mb-5 text-lg font-medium">{tab.title}</div>
-              <div className="mb-5 font-normal">{tab.content}</div>
-              <div className="mb-5 font-normal text-black-light">{tab.content2}</div>
-              <div>{tab.content3}</div>
-            </Tab.Panel>
-          ))}
-        </div>
-      </Tab.Panels>
-    </Tab.Group>
+          </Tab.Panels>
+        </Tab.Group>
+      </form>
+    </FormProvider>
   )
 }
 
