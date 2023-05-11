27c1e0521d5fbd65a224295529640a06f888edd3
david20004000 <david.movsesyan2002@gmail.com>
2023-05-11 14:52:43 +0400

    FIX: Desktop pressing the button does not close the menu

--- src/components/Header/User.tsx
+++ src/components/Header/User.tsx
@@ -23,57 +23,62 @@ export const User = ({ handleLogOut }: IHeaderUserProps) => {
       as="div"
       className="relative"
     >
-      <Menu.Button className="flex h-[52px] w-[52px] items-center justify-center rounded-full bg-white focus:outline-none">
-        <span className="sr-only">Open user menu</span>
-        <UserIcon />
-      </Menu.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-[355px] origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="p-5">
-            <div className="mb-5 flex items-center">
-              <div>
-                <p className="mb-2.5 text-lg text-black">{name}</p>
-                <p className="text-base text-black-light">{email}</p>
-              </div>
-            </div>
-            <ul>
-              {links.map((link) => (
-                <Menu.Item key={link.href}>
-                  <li className="w-100 mb-2.5">
-                    <Link href={link.href}>
+      {({ close }) => (
+        <div>
+          <Menu.Button className="flex h-[52px] w-[52px] items-center justify-center rounded-full bg-white focus:outline-none">
+            <span className="sr-only">Open user menu</span>
+            <UserIcon />
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-[355px] origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="p-5">
+                <div className="mb-5 flex items-center">
+                  <div>
+                    <p className="mb-2.5 text-lg text-black">{name}</p>
+                    <p className="text-base text-black-light">{email}</p>
+                  </div>
+                </div>
+                <ul>
+                  {links.map((link) => (
+                    <Menu.Item key={link.href}>
+                      <li className="w-100 mb-2.5">
+                        <Link href={link.href}>
+                          <Button
+                            className="w-full"
+                            variant="outlined"
+                            onClick={() => close()}
+                          >
+                            {link.label}
+                          </Button>
+                        </Link>
+                      </li>
+                    </Menu.Item>
+                  ))}
+                  <Menu.Item>
+                    <li className="mb-2.5">
                       <Button
                         className="w-full"
+                        onClick={handleLogOut}
                         variant="outlined"
                       >
-                        {link.label}
+                        Log Out
                       </Button>
-                    </Link>
-                  </li>
-                </Menu.Item>
-              ))}
-              <Menu.Item>
-                <li className="mb-2.5">
-                  <Button
-                    className="w-full"
-                    onClick={handleLogOut}
-                    variant="outlined"
-                  >
-                    Log Out
-                  </Button>
-                </li>
-              </Menu.Item>
-            </ul>
-          </div>
-        </Menu.Items>
-      </Transition>
+                    </li>
+                  </Menu.Item>
+                </ul>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </div>
+      )}
     </Menu>
   )
 }
