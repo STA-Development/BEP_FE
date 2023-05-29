c55752bd41114fd6d2411722d6165435b2b35c3f
DavMovsesyan2002 <51234151+DavMovsesyan2002@users.noreply.github.com>
2023-05-29 18:58:14 +0400

    FEATURE: Improve redirection logic (#92)
    * FEATURE: Improve redirection logic
    
    * resolve comments
    
    * resolve comments

---
+++ src/constants/router.ts
@@ -0,0 +1,3 @@
+export const publicRoutes = ['/profile/settings', '/profile/applications', '/fill-the-form']
+
+export const privateRoutes = ['/login', '/register', '/reset-password']
--- src/manager/axiosInstance.ts
+++ src/manager/axiosInstance.ts
@@ -5,6 +5,8 @@ import { usersMiddleware } from '@redux/slices/users'
 import { viewsMiddleware } from '@redux/slices/views'
 import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
+import { isAuthenticated } from '@utils/authUtils'
+
 const getServerUrl = () => devToolsDefaultConfig?.server
 
 class RequestManager {
@@ -59,7 +61,7 @@ class RequestManager {
                 })}`,
               },
             }))
-          } else {
+          } else if (!isAuthenticated()) {
             dispatch(
               viewsMiddleware.setRedirectionState({ path: '/login', params: '', apply: true })
             )
