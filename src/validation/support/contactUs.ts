7bcaa0ff4dd7e37da62f60897a799a86b6ff2731
alexkocharyan01 <alexkocharyan97@gmail.com>
2023-06-05 20:03:29 +0400

    Remove email regex

--- src/validation/support/contactUs.ts
+++ src/validation/support/contactUs.ts
@@ -1,4 +1,4 @@
-import { emailRegex, fullNameRegex, phoneRegex } from '@constants/contactUs'
+import { fullNameRegex, phoneRegex } from '@constants/contactUs'
 import { Translation } from '@constants/translations'
 import i18next from 'i18next'
 import { object, string } from 'yup'
@@ -12,7 +12,6 @@ export const contactUsValidationSchema = object({
     .required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_NAME) as string),
   email: string()
     .email()
-    .matches(emailRegex, i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_EMAIL_MATCHES) as string)
     .required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_EMAIL) as string),
   phone: string()
     .matches(phoneRegex, i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_PHONE_MATCHES) as string)
