f5d83bf30fcd30bc2581919e78132f4890726e1e
alexkocharyan01 <alexkocharyan97@gmail.com>
2023-06-05 18:08:31 +0400

    Creat constants for contact us validation regex

--- src/validation/support/contactUs.ts
+++ src/validation/support/contactUs.ts
@@ -1,3 +1,4 @@
+import { emailRegex, fullNameRegex, phoneRegex } from '@constants/contactUs'
 import { Translation } from '@constants/translations'
 import i18next from 'i18next'
 import { object, string } from 'yup'
@@ -5,22 +6,16 @@ import { object, string } from 'yup'
 export const contactUsValidationSchema = object({
   fullName: string()
     .matches(
-      /^[aA-zZ\s]+$/,
+      fullNameRegex,
       i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_NAME_MATCHES) as string
     )
     .required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_NAME) as string),
   email: string()
     .email()
-    .matches(
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_EMAIL_MATCHES) as string
-    )
+    .matches(emailRegex, i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_EMAIL_MATCHES) as string)
     .required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_EMAIL) as string),
   phone: string()
-    .matches(
-      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-      i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_PHONE_MATCHES) as string
-    )
+    .matches(phoneRegex, i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_PHONE_MATCHES) as string)
     .required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_PHONE) as string),
   message: string().required(i18next.t(Translation.PAGE_CONTACT_US_VALIDATION_MESSAGE) as string),
 })
