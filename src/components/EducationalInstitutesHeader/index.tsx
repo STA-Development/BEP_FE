70366639561ccba5be94b093ab6c3a7b31b3c85e
vahea95 <128638273+vahea95@users.noreply.github.com>
2023-06-12 20:01:26 +0400

    Changing breadcrumbs in educational institutes page (#107)

--- src/components/EducationalInstitutesHeader/index.tsx
+++ src/components/EducationalInstitutesHeader/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Container } from '@components/Container'
 import Filters from '@components/Educationalnstitutes/Filters'
 import { FilterIcon } from '@components/Icons/FilterIcon'
+import { PageHeader } from '@components/PageHeader'
 import { Translation } from '@constants/translations'
 import { Button } from '@uiComponents/Button'
 
@@ -15,38 +17,44 @@ export const EducationalInstitutesHeader = () => {
   }
 
   return (
-    <div className="border-b border-gray-thin py-5 xl:py-10">
-      <div className="flex flex-col xl:mb-5 xl:flex-row xl:justify-between">
-        <h1 className="text-xl">{t(Translation.PAGE_EDUCATIONAL_INSTITUTES_MAIN_TITLE)}</h1>
-        <div className="flex flex-row items-center justify-between">
-          <p className="text-base text-black-light">Home / Master</p>
-          <Button
-            variant="text"
-            onClick={openFilter}
-            LeftIcon={FilterIcon}
-            className="xl:hidden"
-          >
-            <p>{`${
-              !active
-                ? t(Translation.PAGE_EDUCATIONAL_INSTITUTES_FILTER_OPEN)
-                : t(Translation.PAGE_EDUCATIONAL_INSTITUTES_FILTER_CLOSE)
-            }`}</p>
-          </Button>
+    <Container>
+      <PageHeader
+        title={t(Translation.PAGE_EDUCATIONAL_INSTITUTES_MAIN_TITLE)}
+        paths={[t(Translation.NAVBAR_HOME), t(Translation.PAGE_EDUCATIONAL_INSTITUTES_MAIN_TITLE)]}
+      />
+      <div className="grid justify-center border-b border-gray-thin py-5 xl:py-10">
+        <div className="flex flex-col xl:mb-5 xl:flex-row xl:justify-between">
+          <div className="flex flex-row items-center justify-between">
+            <Button
+              variant="text"
+              onClick={openFilter}
+              LeftIcon={FilterIcon}
+              className="xl:hidden"
+            >
+              <p>{`${
+                !active
+                  ? t(Translation.PAGE_EDUCATIONAL_INSTITUTES_FILTER_OPEN)
+                  : t(Translation.PAGE_EDUCATIONAL_INSTITUTES_FILTER_CLOSE)
+              }`}</p>
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="w-full xl:hidden">
-        <div
-          className={`w-full flex-col items-start xl:flex xl:flex-row ${active ? `flex` : `hidden`}
+        <div className="w-full xl:hidden">
+          <div
+            className={`w-full flex-col items-start xl:flex xl:flex-row ${
+              active ? `flex` : `hidden`
+            }
            `}
-        >
+          >
+            <Filters />
+          </div>
+        </div>
+
+        <div className="mt-4 hidden w-full flex-col items-center justify-center xl:flex">
           <Filters />
         </div>
       </div>
-
-      <div className="mt-4 hidden w-full flex-col items-center justify-center xl:flex">
-        <Filters />
-      </div>
-    </div>
+    </Container>
   )
 }
 
