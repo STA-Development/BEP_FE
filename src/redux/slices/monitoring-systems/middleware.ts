00f94a240e373e66ccf69c7a39464c779f23ea38
alexkocharyan01 <alexkocharyan97@gmail.com>
2023-06-27 23:12:10 +0400

    Implement Monitoring Systems logic

--- src/redux/slices/monitoring-systems/middleware.ts
+++ src/redux/slices/monitoring-systems/middleware.ts
@@ -1,16 +1,17 @@
 import API from '@axios/API'
 import { IError } from '@axios/authentication/authManagerTypes'
+import { IParamsProps } from '@components/Educationalnstitutes/helper'
 import { AppDispatch } from '@redux/store'
 
 import slice from './slice'
 
 const { setMonitoringEnumsLoading, setMonitoringEnums, setError } = slice.actions
 
-const getMonitoringEnums = () => async (dispatch: AppDispatch) => {
+const getMonitoringEnums = (filters: IParamsProps[]) => async (dispatch: AppDispatch) => {
   try {
     dispatch(setMonitoringEnumsLoading(true))
 
-    const response = await API.monitoring.getMonitoringEnums()
+    const response = await API.monitoring.getMonitoringEnums(filters)
 
     dispatch(setMonitoringEnums(response.data.data))
   } catch (error) {
