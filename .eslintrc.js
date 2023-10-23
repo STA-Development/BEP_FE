8866b2efd96517175289ad62848ae42e35f80966
Zt0 <azat.antonyan65@gmail.com>
2023-10-23 21:02:22 +0400

    deploy N1

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+
   ignorePatterns: ['node_modules', 'dist', 'build', '.next', 'coverage'],
   globals: {
     RequestInit: 'readonly',
